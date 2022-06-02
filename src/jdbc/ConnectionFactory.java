58f906e45838f28b46acf344a6a3260077653df1
LeonardoHSA <leohsanfins@hotmail.com>
2022-06-02 10:10:55 -0300

    Criando a conexão com o banco

---
+++ src/jdbc/ConnectionFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author multiway
+ */
+public class ConnectionFactory {
+    
+    public Connection conecta(){
+        
+        try {
+            
+            return DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/projetoPOO", "postgres", "batata"); //String de conexão
+            
+        } catch (SQLException e) {
+            
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/projetopoo/ProjetoPOO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetopoo;
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import jdbc.ConnectionFactory;
+
+/**
+ *
+ * @author multiway
+ */
+public class ProjetoPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         try {
+            JOptionPane.showMessageDialog(null, "Testando a conexão");
+            Connection con = new ConnectionFactory().conecta();
+            JOptionPane.showMessageDialog(null, "Está conectado");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar " + e);
+        }
+    }
+    
+}
